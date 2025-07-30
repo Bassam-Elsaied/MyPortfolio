32250862cf779712e7004e0aec3e0ac65b438a22
Bassam-Elsaied <bassame993@gmail.com>
2025-07-30 15:51:56 +0300

    fix content width

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -88,7 +88,7 @@ export default function page() {
   return (
     <div className="h-full bg-primary/30">
       <div className="container mx-auto py-32 text-center xl:text-left h-full flex items-center justify-center">
-        <div className="flex flex-col w-full max-w-700px ">
+        <div className="flex flex-col w-full max-w-[800px] ">
           <motion.h2
             variants={fadeIn("up", 0.2)}
             initial="hidden"
